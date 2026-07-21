98338bcfba956d9d53d81416676c8422ab39308d
fernandososter <fernando.soster@gmail.com>
2026-07-21 18:50:59 -0400

    updating

--- src/sleep_rswa/training/engine.py
+++ src/sleep_rswa/training/engine.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import numpy as np
 import torch
 from torch.nn.utils import clip_grad_norm_
-from .distribution import StageDistribution
+from ..distribution import StageDistribution
 
 from ..metrics import rswa_metrics, staging_metrics
 from .losses import RSWALoss, StagingLoss
