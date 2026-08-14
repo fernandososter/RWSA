809d7c6431dd8ac7da6e98be8199ab48babdfcf1
fernandososter <fernando.soster@gmail.com>
2026-08-13 21:09:50 -0300

    aumentando o SE do EMG

--- src/sleep_rswa/config.py
+++ src/sleep_rswa/config.py
@@ -26,7 +26,7 @@ class ModelConfig:
     eog_kernels: tuple[int, ...] = (50, 150, 250)
     emg_kernels: tuple[int, ...] = (10, 30, 70)
     rswa_emg_in_channels: int = 1
-    rswa_emg_filters: int = 64
+    rswa_emg_filters: int = 128
     rswa_stage_conditioning: bool = False
     rswa_stage_conditioning_detach: bool = True
     rswa_use_baseline_relative_channel: bool = False
